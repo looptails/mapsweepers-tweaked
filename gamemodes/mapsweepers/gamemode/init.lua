42d28089787a52ea56fe39c2ca55e994537f373b
looptit <55616170+looptit@users.noreply.github.com>
2025-09-03 08:22:01 -0500

    fix difficulty scaling being bugged

--- gamemodes/mapsweepers/gamemode/init.lua
+++ gamemodes/mapsweepers/gamemode/init.lua
@@ -560,7 +560,8 @@ end
 
 	function jcms.runprogress_CalculateDifficultyFromWinstreak(winstreak, totalWins)
 		local newPlayerScalar = 1 - math.max((6 - totalWins), 0) * 0.06
-		local final = math.min(((0.9 + winstreak * 0.175) * newPlayerScalar),2) -- difficulty caps out at 200% to prevent unfair gameplay at ultra high winstreaks, may increase in the future
+		local arghh = (0.9 + winstreak * 0.175) * newPlayerScalar
+		local final = math.min(arghh, 3)
 		game.GetWorld():SetNWFloat("jcms_difficulty", final)
 		return final
 		
